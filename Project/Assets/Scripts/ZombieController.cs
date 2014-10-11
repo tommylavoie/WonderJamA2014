6c189ff06a5a0ac1cfd1ca2fa3c56e4e9c5c9a68
SonicYouth <kekout3@gmail.com>
2014-10-10 20:26:59 -0400

    Personnage + Zombie Scripts v1

---
+++ Project/Assets/Scripts/ZombieController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieController : Personnage {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            base.MoveRight();
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            base.MoveLeft();
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            base.MoveForward();
+        }
+
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            base.MoveBackward();
+        }
+	}
+}
