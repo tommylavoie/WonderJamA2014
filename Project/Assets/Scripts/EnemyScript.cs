0a94355275e489266a9b45275f6ad9f1a27d004a
leorux94 <fripouille_16@hotmail.com>
2014-10-10 21:09:34 -0400

    Script EnemyScript

--- Project/Assets/Scripts/EnemyScript.cs
+++ Project/Assets/Scripts/EnemyScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class EnemyScript : Personnage {
 
     int vie;
     int force;
@@ -15,8 +15,15 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        
-        
+        // Randomly choose the enemy type and set it stats
+        type = Random.Range(0, 2);
+        switch (type)
+        {
+            // setStats(vie, Attack, speed)
+            case 0: base.setStats(3, 1, 4); break;
+            case 1: base.setStats(10, 1, 1); break;
+            case 2: base.setStats(5, 3, 1); break;
+        }
 	}
 	
 	// Update is called once per frame
@@ -25,9 +32,26 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	}
 
-    // Function
-    void Deplacement() { 
-        Random.Range(0, 4)
+    // Function permettant le déplacement d'un ennemie
+    void Action() {
+        if (!isThereNearbyPlayer()) { // Attack if true, move if false
+            int direction = Random.Range(0, 4);
+            switch (direction)
+            {
+                case 0: base.MoveRight(); break;
+                case 1: base.MoveBackward(); break;
+                case 2: base.MoveForward(); break;
+                case 3: base.MoveLeft(); break;
+            }
+        }
+        else // ATTACK THE PLAYER
+        {
+            //base.Attack();
+        }
+    }
+
+    bool isThereNearbyPlayer() {
+        return false; // Temporaire
     }
 
 
--- Project/Assets/Scripts/Personnage.cs
+++ Project/Assets/Scripts/Personnage.cs
@@ -14,7 +14,7 @@ public class Personnage : MonoBehaviour {
 	
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame  
 	void Update () 
     {
 	}
