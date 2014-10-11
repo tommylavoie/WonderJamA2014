ea1b5eedf199c7b2d7d532aae170423ed937e58b
leorux94 <fripouille_16@hotmail.com>
2014-10-11 00:10:07 -0400

    modif EnemyScript

--- Project/Assets/Scripts/ZombieController.cs
+++ Project/Assets/Scripts/ZombieController.cs
@@ -5,7 +5,9 @@ public class ZombieController : Personnage {
 
 	// Use this for initialization
 	void Start () {
-	
+        // setStats(vie, Attack, speed)
+        setStats(10, 2, 4);
+        setName("Player");
 	}
 	
 	// Update is called once per frame
