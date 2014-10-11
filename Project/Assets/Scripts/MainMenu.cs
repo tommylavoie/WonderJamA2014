555b3f4e0acc38715a05e450f5d2951256d85bf6
SonicYouth <kekout3@gmail.com>
2014-10-11 04:10:17 -0400

    Images Zombies

--- Project/Assets/Scripts/MainMenu.cs
+++ Project/Assets/Scripts/MainMenu.cs
@@ -7,10 +7,16 @@ public class MainMenu : MonoBehaviour {
 
     void OnGUI()
     {
-        Rect rect = new Rect(Screen.width * 0.5f, Screen.height * 0.5f, Screen.width * 0.5f, Screen.height * 0.1f);
+        Rect rect = new Rect(Screen.width * 0.25f, Screen.height * 0.5f, Screen.width * 0.5f, Screen.height * 0.1f);
         if(GUI.Button(rect, "Jouer"))
         {
 
+        }
+
+        Rect rect2 = new Rect(Screen.width * 0.25f, Screen.height * 0.65f, Screen.width * 0.5f, Screen.height * 0.1f);
+        if (GUI.Button(rect2, "Quitter"))
+        {
+            Application.Quit();
         }
     }
 }
