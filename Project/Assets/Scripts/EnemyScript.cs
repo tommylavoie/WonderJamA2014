a26ef04fae8425172ffa74f72ce1db800e73befa
leorux94 <fripouille_16@hotmail.com>
2014-10-10 20:31:04 -0400

    a

---
+++ Project/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    int vie;
+    int force;
+    int déplacement;
+    int type; 
+    /* 0 = rapide mais faible (dep4, 
+     * 1 = tank
+     * 2 = Lent mais tres fort
+    */
+
+
+	// Use this for initialization
+	void Start () {
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+    // Function
+    void Deplacement() { 
+        Random.Range(0, 4)
+    }
+
+
+}
