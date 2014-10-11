3e2c31ef028a52ca5ec9eeb94ede52b9f53875f9
tommylavoie <tom.patate@hotmail.com>
2014-10-10 22:35:19 -0400

    Tile générés

--- Project/Assets/Scripts/MapGenerator.cs
+++ Project/Assets/Scripts/MapGenerator.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 
 public class MapGenerator : MonoBehaviour 
 {
 	TileManager tileManager;
+	public GameObject groundTile;
+	public GameObject greenTile;
 
 	// Use this for initialization
 	void Start () 
@@ -14,13 +17,24 @@ public class MapGenerator : MonoBehaviour
 
 	void generateTile()
 	{
+		float initialX = transform.position.x-95;
+		float initialY = transform.position.y-95;
 		for(int i=0;i<20;i++)
 		{
 			for(int j=0;j<20;j++)
 			{
 				Tile tile = new Tile();
 				tileManager.changeTile(i,j,tile);
-				Debug.Log(i + " : " + j);
+				if(j %2 == 0)
+				{
+					Instantiate(groundTile);
+					groundTile.transform.position = new Vector3(initialX+(10*j),initialY+(10*i), 0);
+				}
+				else
+				{
+					Instantiate(greenTile);
+					greenTile.transform.position = new Vector3(initialX+(10*j),initialY+(10*i), 0);
+				}
 			}
 		}
 	}
