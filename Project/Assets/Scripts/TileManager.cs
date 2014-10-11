60437dbbdf5bda81469b2d371959d97c66428b3b
tommylavoie <tom.patate@hotmail.com>
2014-10-10 21:06:05 -0400

    Entité et Grille

---
+++ Project/Assets/Scripts/TileManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class TileManager
+{
+	Tile[][] tiles;
+
+	private static TileManager Instance;
+
+	public static TileManager getInstance()
+	{
+		if(Instance == null)
+		{
+			Instance = new TileManager();
+		}
+		return Instance;
+	}
+
+	private TileManager()
+	{
+		tiles = new Tile[20][];
+		for(int i=0;i<20;i++)
+		{
+			tiles[i] = new Tile[20];
+		}
+	}
+
+	public Tile getTile(int x, int y)
+	{
+		return tiles[x][y];
+	}
+
+	public void changeTile(int x, int y, Tile tile)
+	{
+		tiles[x][y] = tile;
+	}
+}
