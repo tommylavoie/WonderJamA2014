b65b4c7ce091d099f9dee31f3ffcc654689357ac
leorux94 <fripouille_16@hotmail.com>
2014-10-12 02:25:48 -0400

    :trollface:

---
+++ Project/Assets/Scripts/SoundScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundScript : MonoBehaviour {
+
+    public static SoundScript Instance;
+
+    public AudioClip powerUpSound;
+    public AudioClip playerAttackSound;
+    public AudioClip enemyAttackSound;
+    public AudioClip playerDeathSound;
+
+	void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("Multiple instances of SoundScript!");
+        }
+        Instance = this;
+    }
+
+    public void MakePowerUpSound()
+    {
+        MakeSound(powerUpSound);
+    }
+
+    public void MakePlayerAttackSound()
+    {
+        MakeSound(playerAttackSound);
+    }
+
+    public void MakeenemyAttackSound()
+    {
+        MakeSound(enemyAttackSound);
+    }
+
+    public void MakePlayerDeathSound()
+    {
+        MakeSound(playerDeathSound);
+    }
+
+    private void MakeSound(AudioClip soundClip)
+    {
+        AudioSource.PlayClipAtPoint(soundClip, transform.position);
+    }
+    
+}
