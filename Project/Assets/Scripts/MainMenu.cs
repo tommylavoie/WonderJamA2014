ec8d3becf69d4ba9db742a31ff5b888f1e702ff5
leorux94 <fripouille_16@hotmail.com>
2014-10-11 22:53:46 -0400

    Ajout du tutoriel

--- Project/Assets/Scripts/MainMenu.cs
+++ Project/Assets/Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ public class MainMenu : MonoBehaviour {
     public Texture background;
     public string button1Text;
     public string button2Text;
+    public string button3Text = "Tutoriel";
 
     void OnGUI()
     {
@@ -13,13 +14,20 @@ public class MainMenu : MonoBehaviour {
         {
             GUI.DrawTexture(new Rect((Screen.width - background.width) / 2, (Screen.height - background.height) / 2, background.width, background.height), background);
         }
-        Rect rect = new Rect(Screen.width * 0.35f, Screen.height * 0.5f, Screen.width * 0.3f, Screen.height * 0.1f);
+
+        Rect rect = new Rect(Screen.width * 0.35f, Screen.height * 0.45f, Screen.width * 0.3f, Screen.height * 0.1f);
         if (GUI.Button(rect, button1Text))
         {
             Application.LoadLevel("Main");
         }
 
-        Rect rect2 = new Rect(Screen.width * 0.35f, Screen.height * 0.65f, Screen.width * 0.3f, Screen.height * 0.1f);
+        Rect rect3 = new Rect(Screen.width * 0.35f, Screen.height * 0.60f, Screen.width * 0.3f, Screen.height * 0.1f);
+        if (GUI.Button(rect3, button3Text))
+        {
+            Application.LoadLevel("Tuto");
+        }
+
+        Rect rect2 = new Rect(Screen.width * 0.35f, Screen.height * 0.75f, Screen.width * 0.3f, Screen.height * 0.1f);
         if (GUI.Button(rect2, button2Text))
         {
             Application.Quit();
